17eeffde15eea730ad0ccddcdc6ac5e9f69506fa
Rinat Khafizov <freerinch@gmail.com>
2023-03-03 18:25:19 +0800

    Change main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from pprint import pprint
-
+#Задание 1
 current = os.getcwd()
 folder = '2.4.files'
 file = 'recipes.txt'
@@ -8,10 +8,11 @@ full_path = os.path.join(current, folder, file)
 
 with open(full_path) as file:
     cook_book = dict()
-    ingridients = list()
+
     for line in file:
         dish_name = line.strip()
         dishes_count = int(file.readline())
+        ingridients = list()
         for _ in range(dishes_count):
             ing = file.readline().strip()
             name_of_ing, pices, measure = ing.split(' | ')
@@ -21,26 +22,33 @@ with open(full_path) as file:
                  'measure': measure})
         cook_book[dish_name] = ingridients
         file.readline()
-pprint(cook_book,sort_dicts=False)
 
-# def get_shop_list_by_dishes(dishes, person_count):
-#     ingr_list = dict()
-#
-#     for dish_name in dishes:
-#         if dish_name in cook_book:
-#             for ings in cook_book[dish_name]:
-#                 quantity_list = dict()
-#                 if ings['name_of_ing'] not in ingr_list:
-#                     quantity_list['measure'] = ings['measure']
-#                     quantity_list['pices'] = ings['pices'] * person_count
-#                     ingr_list[ings['ingredient_name']] = quantity_list
-#                 else:
-#                     ingr_list[ings['name_of_ing']]['quantity'] = ingr_list[ings['name_of_ing']]['pices'] + \
-#                                                                  ings['pices'] * person_count
-#
-#         else:
-#             print(f'\n"Такого блюда нет в списке!"\n')
-#     return ingr_list
-# # print(get_shop_list_by_dishes('Оливье',2))
-#
-# pprint(cook_book,sort_dicts=False)
+
+# {get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+#   'Картофель': {'measure': 'кг', 'quantity': 2},
+#   'Молоко': {'measure': 'мл', 'quantity': 200},
+#   'Помидор': {'measure': 'шт', 'quantity': 4},
+#   'Сыр гауда': {'measure': 'г', 'quantity': 200},
+#   'Яйцо': {'measure': 'шт', 'quantity': 4},
+#   'Чеснок': {'measure': 'зубч', 'quantity': 6}
+# }
+# Задание 2
+
+def get_shop_list_by_dishes(dishes, person):
+    total = dict()
+    for dish in dishes:
+        if dish in cook_book:
+            for ing in cook_book[dish_name]:
+                quan_list = dict()
+                if ing['name_of_ing'] not in total:
+                    quan_list['measure'] = ing['measure']
+                    quan_list['pices'] = ing['pices'] * person
+                    total[ing['name_of_ing']] = quan_list
+                else:
+                    total[ing['name_of_ing']]['pices'] = total[ing['name_of_ing']]['pices'] + \
+                                                         ing['pices'] * person
+    return total
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
+
