2bdbcbb015b5e97259be2c2c76aa8e4b7caf726e
Rinat Khafizov <freerinch@gmail.com>
2023-03-05 19:55:58 +0800

    Change main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 from pprint import pprint
 #Задание 1
+
 current = os.getcwd()
 folder = '2.4.files'
 file = 'recipes.txt'
 full_path = os.path.join(current, folder, file)
+
 
 with open(full_path) as file:
     cook_book = dict()
@@ -23,32 +25,43 @@ with open(full_path) as file:
         cook_book[dish_name] = ingridients
         file.readline()
 
+# pprint(cook_book, sort_dicts=False)
 
-# {get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
-#   'Картофель': {'measure': 'кг', 'quantity': 2},
-#   'Молоко': {'measure': 'мл', 'quantity': 200},
-#   'Помидор': {'measure': 'шт', 'quantity': 4},
-#   'Сыр гауда': {'measure': 'г', 'quantity': 200},
-#   'Яйцо': {'measure': 'шт', 'quantity': 4},
-#   'Чеснок': {'measure': 'зубч', 'quantity': 6}
-# }
 # Задание 2
 
 def get_shop_list_by_dishes(dishes, person):
     total = dict()
     for dish in dishes:
         if dish in cook_book:
-            for ing in cook_book[dish_name]:
+            for ing in cook_book[dish]:
                 quan_list = dict()
+                quan_list['pices'] = int(ing['pices']) * person
                 if ing['name_of_ing'] not in total:
                     quan_list['measure'] = ing['measure']
-                    quan_list['pices'] = ing['pices'] * person
                     total[ing['name_of_ing']] = quan_list
                 else:
                     total[ing['name_of_ing']]['pices'] = total[ing['name_of_ing']]['pices'] + \
-                                                         ing['pices'] * person
+                                                         int(ing['pices']) * person
     return total
 
-print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
 
+# pprint(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2))
 
+# Задание 3
+
+files = ['1.txt', '2.txt', '3.txt']
+os.chdir('2.4.files/sorted')
+massive = []
+
+for file_name in files:
+    with open(file_name) as f:
+        file_data = f.readlines()
+        file_data.insert(0, f'Количество строк {str(len(file_data))}\n')
+        file_data.insert(0, f'\n Название файла: {file_name}\n')
+        massive.append(file_data)
+
+massive.sort(key=len)
+with open('total_file.txt', 'a') as document:
+    for text in massive:
+        with open('total_file.txt', 'a') as document:
+            document.writelines(text)
